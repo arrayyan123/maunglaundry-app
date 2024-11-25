b53daa5e2fd6ca352685ea69cb5fdf11c28651de
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2024-11-25 14:21:36 +0700

    entah

--- resources/js/Components/Geolocation.jsx
+++ resources/js/Components/Geolocation.jsx
@@ -15,7 +15,6 @@ const DistanceCalculator = () => {
         Math.cos((lat2 * Math.PI) / 180) *
         Math.sin(dLon / 2) *
         Math.sin(dLon / 2);
-
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     return earthRadius * c; 
   };
